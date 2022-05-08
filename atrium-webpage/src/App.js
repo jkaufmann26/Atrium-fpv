9448c06e32fb57893afd6321d48f436cfaf7c900
jkaufmann26 <52751856+jkaufmann26@users.noreply.github.com>
2022-05-08 13:11:49 -0600

    Start

---
+++ atrium-webpage/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="container">
+        <div id = "title" class="line-text">Atrium FPV</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ atrium-webpage/src/Tabs.js
@@ -0,0 +1,74 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import './style.css';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function BasicTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%' , transitionProperty: 'all'}}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="ABOUT ME" {...a11yProps(0)} />
+          <Tab label="SHOTS" {...a11yProps(1)} />
+          <Tab label="CONTACT" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>      
+      <body id='test'>
+          <div className= 'body'>I'm an up and coming FPV Drone Pilot looking to make a name for himself.</div>
+      </body>
+        <div className="video-responsive">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/oh5IbKRdQFQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+    </Box>
+  );
+}
